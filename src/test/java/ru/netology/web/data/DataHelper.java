63d6e023d8d50843aa79094c75004cedf47bd1dc
Nadezhda <7_nadezhda@mail.ru>
2023-06-07 22:09:50 +0400

    fix datahelper

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -15,22 +15,24 @@ public class DataHelper {
     }
 
     public static AuthInfo getAuthInfo() {
+
         return new AuthInfo("vasya", "qwerty123");
     }
 
 
-
     public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+
         return new AuthInfo("petya", "123qwerty");
     }
 
-    public static CardInfo getCard1 () {
+    public static CardInfo getCard1() {
         return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
     }
 
-    public static CardInfo getCard2 () {
+    public static CardInfo getCard2() {
         return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
+
     public static int getValidAmount(int balance) {
         return new Random().nextInt(balance) + 1;
     }
@@ -38,6 +40,7 @@ public class DataHelper {
     public static int getInvalidAmount(int balance) {
         return Math.abs(balance) + new Random().nextInt(10000);
     }
+
     @Value
     public static class VerificationCode {
         private String code;
